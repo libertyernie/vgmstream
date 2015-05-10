17fbbb99f82fc0bbbf9fe38a2d0a775b6864e9f9
libertyernie <maybeway36@gmail.com>
2015-05-09 21:28:51 -0500

    Start work on a GUI version of exportloop

---
+++ exportloop_gui/exportloop_gui.cpp
@@ -0,0 +1,16 @@
+// exportloop_gui.cpp : main project file.
+
+#include "stdafx.h"
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace exportloop_gui;
+
+[STAThread]
+int main(array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MainForm());
+}
