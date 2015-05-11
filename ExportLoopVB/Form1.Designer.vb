ad0c25695cebe0258b9547d1147a9cae7d7d7c7d
libertyernie <maybeway36@gmail.com>
2015-05-10 20:23:43 -0500

    Import BRSTM to WAV Converter

---
+++ ExportLoopVB/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtOutputDir = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btnOpenFolder = New System.Windows.Forms.Button()
+        Me.chk0ToStart = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.chkStartToEnd = New System.Windows.Forms.CheckBox()
+        Me.chk0ToEnd = New System.Windows.Forms.CheckBox()
+        Me.btnAbout = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.AllowDrop = True
+        Me.ListView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListView1.Location = New System.Drawing.Point(12, 12)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(279, 187)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.List
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.Location = New System.Drawing.Point(297, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRemove.Location = New System.Drawing.Point(297, 41)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemove.TabIndex = 2
+        Me.btnRemove.Text = "Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.Location = New System.Drawing.Point(12, 300)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 20)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Output directory:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'txtOutputDir
+        '
+        Me.txtOutputDir.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtOutputDir.Location = New System.Drawing.Point(103, 300)
+        Me.txtOutputDir.Name = "txtOutputDir"
+        Me.txtOutputDir.Size = New System.Drawing.Size(157, 20)
+        Me.txtOutputDir.TabIndex = 8
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBrowse.Location = New System.Drawing.Point(266, 299)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(50, 21)
+        Me.btnBrowse.TabIndex = 9
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnStart.Location = New System.Drawing.Point(297, 326)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 12
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Multiselect = True
+        '
+        'btnOpenFolder
+        '
+        Me.btnOpenFolder.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnOpenFolder.Location = New System.Drawing.Point(322, 299)
+        Me.btnOpenFolder.Name = "btnOpenFolder"
+        Me.btnOpenFolder.Size = New System.Drawing.Size(50, 21)
+        Me.btnOpenFolder.TabIndex = 10
+        Me.btnOpenFolder.Text = "Open"
+        Me.btnOpenFolder.UseVisualStyleBackColor = True
+        '
+        'chk0ToStart
+        '
+        Me.chk0ToStart.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.chk0ToStart.AutoSize = True
+        Me.chk0ToStart.Location = New System.Drawing.Point(15, 231)
+        Me.chk0ToStart.Name = "chk0ToStart"
+        Me.chk0ToStart.Size = New System.Drawing.Size(132, 17)
+        Me.chk0ToStart.TabIndex = 4
+        Me.chk0ToStart.Text = "Song start to loop start"
+        Me.chk0ToStart.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label2.Location = New System.Drawing.Point(12, 205)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Files to export:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'chkStartToEnd
+        '
+        Me.chkStartToEnd.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.chkStartToEnd.AutoSize = True
+        Me.chkStartToEnd.Checked = True
+        Me.chkStartToEnd.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkStartToEnd.Location = New System.Drawing.Point(15, 254)
+        Me.chkStartToEnd.Name = "chkStartToEnd"
+        Me.chkStartToEnd.Size = New System.Drawing.Size(106, 17)
+        Me.chkStartToEnd.TabIndex = 5
+        Me.chkStartToEnd.Text = "Loop start to end"
+        Me.chkStartToEnd.UseVisualStyleBackColor = True
+        '
+        'chk0ToEnd
+        '
+        Me.chk0ToEnd.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.chk0ToEnd.AutoSize = True
+        Me.chk0ToEnd.Checked = True
+        Me.chk0ToEnd.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chk0ToEnd.Location = New System.Drawing.Point(15, 277)
+        Me.chk0ToEnd.Name = "chk0ToEnd"
+        Me.chk0ToEnd.Size = New System.Drawing.Size(168, 17)
+        Me.chk0ToEnd.TabIndex = 6
+        Me.chk0ToEnd.Text = "Song start to end (entire song)"
+        Me.chk0ToEnd.UseVisualStyleBackColor = True
+        '
+        'btnAbout
+        '
+        Me.btnAbout.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAbout.Location = New System.Drawing.Point(12, 326)
+        Me.btnAbout.Name = "btnAbout"
+        Me.btnAbout.Size = New System.Drawing.Size(75, 23)
+        Me.btnAbout.TabIndex = 11
+        Me.btnAbout.Text = "About"
+        Me.btnAbout.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 361)
+        Me.Controls.Add(Me.btnAbout)
+        Me.Controls.Add(Me.chk0ToEnd)
+        Me.Controls.Add(Me.chkStartToEnd)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.chk0ToStart)
+        Me.Controls.Add(Me.btnOpenFolder)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtOutputDir)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.ListView1)
+        Me.Name = "Form1"
+        Me.Text = "BRSTM to WAV Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnRemove As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtOutputDir As System.Windows.Forms.TextBox
+    Friend WithEvents btnBrowse As System.Windows.Forms.Button
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents btnOpenFolder As System.Windows.Forms.Button
+    Friend WithEvents chk0ToStart As System.Windows.Forms.CheckBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents chkStartToEnd As System.Windows.Forms.CheckBox
+    Friend WithEvents chk0ToEnd As System.Windows.Forms.CheckBox
+    Friend WithEvents btnAbout As System.Windows.Forms.Button
+
+End Class
---
+++ ExportLoopVB/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.IO
+Imports BrawlLib.SSBB.ResourceNodes
+Imports System.Audio
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtOutputDir.Text = Environment.CurrentDirectory
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            For Each f In OpenFileDialog1.FileNames
+                ListView1.Items.Add(f)
+            Next
+        End If
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        For Each item As ListViewItem In ListView1.SelectedItems
+            ListView1.Items.Remove(item)
+        Next
+    End Sub
+
+    Private Sub ListView1_DragEnter(sender As Object, e As DragEventArgs) Handles ListView1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Link
+        End If
+    End Sub
+
+    Private Sub ListView1_DragDrop(sender As Object, e As DragEventArgs) Handles ListView1.DragDrop
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            Dim filenames As String() = e.Data.GetData(DataFormats.FileDrop)
+            For Each filename As String In filenames
+                ListView1.Items.Add(filename)
+            Next
+        End If
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If Not String.IsNullOrEmpty(txtOutputDir.Text) Then
+            FolderBrowserDialog1.SelectedPath = txtOutputDir.Text
+            If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+                txtOutputDir.Text = FolderBrowserDialog1.SelectedPath
+            End If
+        End If
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If Not Directory.Exists(txtOutputDir.Text) Then
+            MessageBox.Show("The specified output directory does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim wavsPerBrstm As Integer
+        If chk0ToStart.Checked Then
+            wavsPerBrstm += 1
+        End If
+        If chkStartToEnd.Checked Then
+            wavsPerBrstm += 1
+        End If
+        If chk0ToEnd.Checked Then
+            wavsPerBrstm += 1
+        End If
+
+        Using pw As New ProgressWindow(Me, "Progress", "Caption", True)
+            pw.Begin(0, wavsPerBrstm * ListView1.Items.Count, 0)
+
+            For Each item As ListViewItem In ListView1.Items
+                If pw.Cancelled Then
+                    pw.Finish()
+                    Exit For
+                End If
+
+                Using node As ResourceNode = NodeFactory.FromFile(Nothing, item.Text)
+                    If Not TypeOf node Is RSTMNode Then
+                        Continue For
+                    End If
+
+                    Dim brstm = CType(node, RSTMNode)
+                    Dim audioStream = brstm.CreateStreams().First()
+                    If chk0ToStart.Checked Then
+                        Dim filename = txtOutputDir.Text & Path.DirectorySeparatorChar & brstm.Name & " (beginning).wav"
+                        pw.Caption = Path.GetFileName(filename)
+                        WAV.ToFile(audioStream,
+                                   filename,
+                                   0,
+                                   brstm.LoopStartSample)
+                        pw.Update(pw.CurrentValue + 1)
+                    End If
+                    If chkStartToEnd.Checked Then
+                        Dim filename = txtOutputDir.Text & Path.DirectorySeparatorChar & brstm.Name & " (loop).wav"
+                        pw.Caption = Path.GetFileName(filename)
+                        WAV.ToFile(audioStream,
+                                   filename,
+                                   brstm.LoopStartSample)
+                        pw.Update(pw.CurrentValue + 1)
+                    End If
+                    If chk0ToEnd.Checked Then
+                        Dim filename = txtOutputDir.Text & Path.DirectorySeparatorChar & brstm.Name & ".wav"
+                        pw.Caption = Path.GetFileName(filename)
+                        WAV.ToFile(audioStream,
+                                   filename)
+                        pw.Update(pw.CurrentValue + 1)
+                    End If
+                End Using
+            Next
+        End Using
+    End Sub
+
+    Private Sub btnOpenFolder_Click(sender As Object, e As EventArgs) Handles btnOpenFolder.Click
+        If Not Directory.Exists(txtOutputDir.Text) Then
+            MessageBox.Show("The specified output directory does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Process.Start(txtOutputDir.Text)
+    End Sub
+
+    Private Sub btnAbout_Click(sender As Object, e As EventArgs) Handles btnAbout.Click
+        Dim N = Environment.NewLine
+        MessageBox.Show("BRSTM to WAV Converter" & N & "© 2015 libertyernie" & N & N & "https://github.com/libertyernie/BrstmToWav" & N & N &
+                        "This program is provided as-is without any warranty, implied or otherwise. By using this program, the end user agrees to take full responsibility regarding its proper and lawful use. The authors/hosts/distributors cannot be held responsible for any damage resulting in the use of this program, nor can they be held accountable for the manner in which it is used.")
+    End Sub
+End Class
