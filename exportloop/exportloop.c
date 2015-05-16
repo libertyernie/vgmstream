c245b6b7e784e2a1c9539273a072894f750cba11
libertyernie <maybeway36@gmail.com>
2015-05-16 15:49:27 -0500

    Add license notice to exportloop.c

--- exportloop/exportloop.c
+++ exportloop/exportloop.c
@@ -39,8 +39,20 @@ char* remove_extension_and_path(const char* input) {
 }
 
 int usage(int retval, char* name) {
-	fprintf(stderr, "exportloop\n"
+	fprintf(stderr, "exportloop 1.1\n"
 		"https://github.com/libertyernie/vgmstream\n"
+		"\n"
+		"Permission to use, copy, modify, and distribute this software for any " 
+		"purpose with or without fee is hereby granted, provided that the above " 
+		"copyright notice and this permission notice appear in all copies.\n"
+		"\n"
+		"THE SOFTWARE IS PROVIDED ""AS IS"" AND THE AUTHOR DISCLAIMS ALL WARRANTIES " 
+		"WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF " 
+		"MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR " 
+		"ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES " 
+		"WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN " 
+		"ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF " 
+		"OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.\n"
 		"\n"
 		"Usage:\n"
 		"exportloop [options] {files ...}\n"
